f6fb923cc491825b5cf1e355de8cc468e79a36c0
Kevin Duraj <kduraj@gmail.com>
2016-07-11 16:31:06 -0700

    2016-07-11 16:31

---
+++ analyze-log/run.sh
@@ -0,0 +1,10 @@
+# Remove short lines
+sed -i '/^.\{,16\}$/d' /var/log/httpd/access_log
+
+spark-shell             \
+  --driver-memory 8G    \
+  --executor-memory 2G  \
+  --executor-cores 4    \
+  -i analyze-log.scala
+
+
