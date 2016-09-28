29bd873e1e43b804658b075c878cf13a2127bafd
Kevin Duraj <kduraj@gmail.com>
2016-09-27 22:50:21 -0700

    2016-09-27 22:50

--- mysql/create_schema.sh
+++ mysql/create_schema.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #-----------------------------------------------------------------------------------#
-DB='engine39'
+DB='engine79'
 SERVER='localhost'
 #-----------------------------------------------------------------------------------#
 #                                 Drop Database
@@ -27,30 +27,30 @@ create_tables()
 SQL="
 CREATE TABLE $DB.part_$1 (
 
-  sha256url 	  char(64) 	      CHARACTER SET ascii NOT NULL,
-  md5root 	    char(32) 	      CHARACTER SET ascii NOT NULL,
+  sha256url 	char(64) 	    CHARACTER SET ascii NOT NULL,
+  md5root       char(32) 	    CHARACTER SET ascii NOT NULL,
 
-  url 		      varchar(255) 	  NOT NULL,
-  root 		      varchar(64) 	  NOT NULL,
+  url 		    varchar(255) 	NOT NULL,
+  root 		    varchar(64) 	NOT NULL,
 
-  tags 		      varchar(128) 	  DEFAULT NULL,
-  title 	      varchar(128) 	  DEFAULT NULL,
-  body 		      varchar(3072) 	DEFAULT NULL,
-  alexa 	      mediumint(6) 	  DEFAULT NULL,
-  rank 		      mediumint(6) 	  DEFAULT NULL,
-  hit1 		      mediumint(6) 	  DEFAULT NULL,
-  hit2 		      mediumint(6) 	  DEFAULT NULL,
-  hit3 		      mediumint(6) 	  DEFAULT NULL,
-  category 	    char(3) 	      DEFAULT NULL,
-  period 	      date 		        DEFAULT NULL,
+  tags 		    varchar(128) 	DEFAULT NULL,
+  title 	    varchar(128) 	DEFAULT NULL,
+  body 		    varchar(3072)   DEFAULT NULL,
+  alexa 	    mediumint(6) 	DEFAULT NULL,
+  rank 		    mediumint(6) 	DEFAULT NULL,
+  hit1 		    mediumint(6) 	DEFAULT NULL,
+  hit2 		    mediumint(6) 	DEFAULT NULL,
+  hit3 		    mediumint(6) 	DEFAULT NULL,
+  category 	    char(3) 	    DEFAULT NULL,
+  period 	    date 		    DEFAULT NULL,
 
   gunning       float(5,2)      NOT NULL DEFAULT '0.00', 
   flesch        float(5,2)      NOT NULL DEFAULT '0.00', 
   kincaid       float(5,2)      NOT NULL DEFAULT '0.00', 
   
   sentence      smallint(5)	    UNSIGNED NOT NULL DEFAULT '0',
-  words 	      float(5,2) 	    UNSIGNED NOT NULL DEFAULT '0.00',
-  syllables 	  float(7,6) 	    UNSIGNED NOT NULL DEFAULT '0.000000',
+  words 	    float(5,2) 	    UNSIGNED NOT NULL DEFAULT '0.00',
+  syllables 	float(7,6) 	    UNSIGNED NOT NULL DEFAULT '0.000000',
   complex     	float(4,2) 	    UNSIGNED NOT NULL DEFAULT '0.00'
 
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
--- mysql/transformation/1-add-engine2engine.sh
+++ mysql/transformation/1-add-engine2engine.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 #------------------------------------------------------------------------------------#
+# mysql_config_editor set --login-path=local --host=localhost --user=root --password
+#------------------------------------------------------------------------------------#
 USERNAME='root'
-DATABASES='engine40'
-DESTINATION='engine37'
+DATABASES='engine78'
+DESTINATION='engine77'
 #------------------------------------------------------------------------------------#
 
 for DATABASE in $DATABASES
 do  
-  TABLE_LIST=`mysql -u $USERNAME -p$PASSWORD -NB -e "SHOW TABLES FROM $DATABASE"`
+  TABLE_LIST=`mysql --login-path=local -NB -e "SHOW TABLES FROM $DATABASE"`
 
   for E in $TABLE_LIST
   do
       SQL="INSERT LOW_PRIORITY INTO $DESTINATION.$E SELECT * FROM $DATABASE.$E "
       echo "$SQL"
-      res1=`mysql -u $USERNAME -p$PASSWORD -NB -e  "$SQL"`
+      res1=`mysql --login-path=local -NB -e  "$SQL"`
       echo "$res1"
-      sleep 3
   done
 done
 
